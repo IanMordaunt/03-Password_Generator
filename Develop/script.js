1378907bf4a91719e4f35d0b4f22c6e4a09d87db
Ian <new@news-MacBook-Pro.local>
2021-12-08 16:54:56 -0700

    Update to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let lowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let specialC = ["~", "!", "@", "#", "%", "^", "" ]
+let result = []
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +15,21 @@ function writePassword() {
 
 }
 
+function
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  let passwordLength = prompt {
+
+  }
+};
+ if (!passwordLength || passwordLength <= 8 || passwordLength >= 128) {
+   return alert
+ }
+ 
+ result.join
+
+ for (let i=0; i <passwordLength; i++) {
+
+ }
