90f5b80ee13ca2a8e4f73441acb5bfeba9b4bf2c
Ian <new@news-MacBook-Pro.local>
2021-12-09 21:07:39 -0700

    Updated: Assets, Develop, README.md

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,157 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let lowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let specialC = ["~", "!", "@", "#", "%", "^", "" ]
-let result = []
+let lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialC = [
+  "~",
+  "!",
+  "@",
+  "#",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "+",
+  "=",
+  "?",
+  ">",
+  "<",
+  "[",
+  "]",
+  "{",
+  "}",
+  ".",
+  ";",
+  ":",
+];
 
-// Write password to the #password input
+
+function generatePassword() {
+  
+let output = [];
+let createdPassword = [];
+
+  let passwordLength = parseInt(prompt(
+    "Enter the number of characters you want for your password. (between 8 and 128 characters)"
+  ));
+
+  if(Number.isNaN(passwordLength)){
+    return alert("Invalid character Length.");
+  }
+
+  if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
+    return alert("Invalid character Length.");
+  }
+
+  console.log(output);
+
+  //  Confirm: to determine lowercase
+  let addlowerC = confirm("Do you want lowercase characters in your password?");
+
+  if (addlowerC === true) {
+    output = output.concat(lowerCase);
+  }
+  
+  console.log(output);
+
+  //  Confirm: to determine uppercase
+  let addupperC = confirm("Do you want uppercase characters in your password?");
+  if (addupperC === true) {
+    output = output.concat(upperCase)
+  }
+  console.log(output);
+
+  //  Confirm: to determine numbers
+  let addnums = confirm("Do you want numbers in your password?");
+  if (addnums === true) {
+    output= output.concat(numbers);
+  }
+  console.log(output);
+
+  //  Confirm: to determine special characters
+  let addspecC = confirm("Do you want special characters in your password?");
+  if (addspecC === true) {
+    output = output.concat(specialC);
+  }
+  console.log(output);
+
+
+  for (let i = 0; i < passwordLength; i++) {
+    var randomFormula= Math.floor(Math.random() * output.length);
+    var randomOutput = output[randomFormula]
+
+    createdPassword.push(randomOutput)
+  }
+  
+
+  return createdPassword.join('');
+
+}
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-function generatePassword() {
-  let passwordLength = prompt {
-
-  }
-};
- if (!passwordLength || passwordLength <= 8 || passwordLength >= 128) {
-   return alert
- }
- 
- result.join
-
- for (let i=0; i <passwordLength; i++) {
-
- }
--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # 03 JavaScript: Password Generator
 
-## Your Task
+## Task:
+Create a password generator
 
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
 
-## User Story
+
+
+## 
 
 ```
 AS AN employee with access to sensitive data
